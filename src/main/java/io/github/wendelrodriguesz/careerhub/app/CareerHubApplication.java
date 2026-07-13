f665c71a57f8e3c9e3e09e5943f07fd50c125cac
WendelRodriguesz <wendeldev2010@gmail.com>
2026-07-13 18:11:09 -0300

    versão inicial do console de menu

--- src/main/java/io/github/wendelrodriguesz/careerhub/app/CareerHubApplication.java
+++ src/main/java/io/github/wendelrodriguesz/careerhub/app/CareerHubApplication.java
@@ -1,9 +1,50 @@
 package io.github.wendelrodriguesz.careerhub.app;
+import java.util.Scanner;
 
 public class CareerHubApplication {
 
     public static void main (String[] args){
-        System.out.println("=== CAREER HUB ===");
-        System.out.println("Sistema iniciado com sucesso!");
+        Scanner scanner = new Scanner(System.in);
+        boolean running = true;
+
+        while(running){
+            System.out.println();
+            System.out.println("====== Bem-vindo ao Career Hub ======");
+            System.out.println("1. Gerenciar perfil");
+            System.out.println("2. Gerenciar projetos");
+            System.out.println("3. Gerenciar experiências");
+            System.out.println("4. Gerenciar formação");
+            System.out.println("5. Gerenciar habilidades");
+            System.out.println("6. Gerenciar links profissionais");
+            System.out.println("7. Gerar prévia do portfólio");
+            System.out.println("0. Sair");
+            System.out.print("Escolha uma opção: ");
+            String operacao = scanner.nextLine();
+
+            switch (operacao){
+                case "1"->
+                    System.out.println("Gerenciamento de perfil ainda não implementado.");
+                case "2"->
+                    System.out.println("2. Gerenciamento de projetos ainda não implementado.");
+                case "3"->
+                    System.out.println("3. Gerenciamento de experiências ainda não implementado.");
+                case "4"->
+                    System.out.println("4. Gerenciamento de formação ainda não implementado.");
+                case "5"->
+                    System.out.println("5. Gerenciamento de habilidades ainda não implementado.");
+                case "6"->
+                    System.out.println("6. Gerenciamento de links profissionais ainda não implementado.");
+                case "7"->
+                    System.out.println("7. Gerenciamento de prévia do portfólio ainda não implementado.");
+                case "0"->{
+                    running = false;
+                    System.out.println("Encerrando o sistema...");
+                    System.out.println("Até logo!");
+                System.out.printf("2d");
+                }
+                default -> System.out.println("Opção inválida. Digite uma opção do menu.");
+            }
+            scanner.close();
+        }
     }
 }
