79e0e11bfe617e48d320ae6cf68fba84f7280144
WendelRodriguesz <wendeldev2010@gmail.com>
2026-07-13 19:18:44 -0300

    feat: Adicionando Getters e Setters e a classe profile.

--- src/main/java/io/github/wendelrodriguesz/careerhub/app/CareerHubApplication.java
+++ src/main/java/io/github/wendelrodriguesz/careerhub/app/CareerHubApplication.java
@@ -1,13 +1,23 @@
 package io.github.wendelrodriguesz.careerhub.app;
+
+import io.github.wendelrodriguesz.careerhub.model.Profile;
+
 import java.util.Scanner;
 
 public class CareerHubApplication {
 
-    public static void main (String[] args){
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        Profile profile = new Profile(
+                "Wendel Rodrigues",
+                "Desenvolvedor Full Stack",
+                "Estudante de Engenharia de Software e desenvolvedor de aplicações web.",
+                "wendeldev2010@gmail.com",
+                "Quixadá, CE"
+        );
         boolean running = true;
 
-        while(running){
+        while (running) {
             System.out.println();
             System.out.println("====== Bem-vindo ao Career Hub ======");
             System.out.println("1. Gerenciar perfil");
@@ -21,30 +31,23 @@ public class CareerHubApplication {
             System.out.print("Escolha uma opção: ");
             String operacao = scanner.nextLine();
 
-            switch (operacao){
-                case "1"->
-                    System.out.println("Gerenciamento de perfil ainda não implementado.");
-                case "2"->
-                    System.out.println("2. Gerenciamento de projetos ainda não implementado.");
-                case "3"->
-                    System.out.println("3. Gerenciamento de experiências ainda não implementado.");
-                case "4"->
-                    System.out.println("4. Gerenciamento de formação ainda não implementado.");
-                case "5"->
-                    System.out.println("5. Gerenciamento de habilidades ainda não implementado.");
-                case "6"->
-                    System.out.println("6. Gerenciamento de links profissionais ainda não implementado.");
-                case "7"->
-                    System.out.println("7. Gerenciamento de prévia do portfólio ainda não implementado.");
-                case "0"->{
+            switch (operacao) {
+                case "1" -> System.out.println(profile.getInfos());
+                case "2" -> System.out.println("Gerenciamento de projetos ainda não implementado.");
+                case "3" -> System.out.println("Gerenciamento de experiências ainda não implementado.");
+                case "4" -> System.out.println("Gerenciamento de formação ainda não implementado.");
+                case "5" -> System.out.println("Gerenciamento de habilidades ainda não implementado.");
+                case "6" -> System.out.println("Gerenciamento de links profissionais ainda não implementado.");
+                case "7" -> System.out.println("Gerenciamento de prévia do portfólio ainda não implementado.");
+                case "0" -> {
                     running = false;
                     System.out.println("Encerrando o sistema...");
                     System.out.println("Até logo!");
-                System.out.printf("2d");
+                    System.out.print("2d");
                 }
                 default -> System.out.println("Opção inválida. Digite uma opção do menu.");
             }
-            scanner.close();
         }
+        scanner.close();
     }
 }
---
+++ src/main/java/io/github/wendelrodriguesz/careerhub/model/Profile.java
@@ -0,0 +1,73 @@
+package io.github.wendelrodriguesz.careerhub.model;
+
+public class Profile {
+    private String name;
+    private String professionalTitle;
+    private String summary;
+    private String email;
+    private String city;
+
+    public Profile(
+            String name,
+            String professionalTitle,
+            String summary,
+            String email,
+            String city
+    ) {
+        this.name = name;
+        this.professionalTitle = professionalTitle;
+        this.summary = summary;
+        this.email = email;
+        this.city = city;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getProfessionalTitle() {
+        return this.professionalTitle;
+    }
+
+    public String getSummary() {
+        return this.summary;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public String getCity() {
+        return this.city;
+    }
+
+    public String getInfos() {
+        return ("\n" +
+                "\n---------- PERFIL ----------" +
+                "\nNome: " + this.getName() +
+                "\nTítulo: " + this.getProfessionalTitle() +
+                "\nResumo: " + this.getSummary() +
+                "\nE-mail: " + this.getEmail() +
+                "\nCidade: " + this.getCity());
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setProfessionalTitle(String professionalTitle) {
+        this.professionalTitle = professionalTitle;
+    }
+
+    public void setSummary(String summary) {
+        this.summary = summary;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+}
