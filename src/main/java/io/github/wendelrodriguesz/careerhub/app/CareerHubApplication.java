453ce41f9a551a536af6f4e44adb326b484e1e86
WendelRodriguesz <wendeldev2010@gmail.com>
2026-07-13 16:22:20 -0300

    Correção para maven

---
+++ src/main/java/io/github/wendelrodriguesz/careerhub/app/CareerHubApplication.java
@@ -0,0 +1,9 @@
+package io.github.wendelrodriguesz.careerhub.app;
+
+public class CareerHubApplication {
+
+    public static void main (String[] args){
+        System.out.println("=== CAREER HUB ===");
+        System.out.println("Sistema iniciado com sucesso!");
+    }
+}
